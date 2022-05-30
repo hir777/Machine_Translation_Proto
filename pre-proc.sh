343221a51aefc7bec4fe966d77fff3f5a0fbd54a
hiro <69237236+hir777@users.noreply.github.com>
2022-05-30 18:21:29 +0900

    Update pre-proc.sh

--- pre-proc.sh
+++ pre-proc.sh
@@ -1,10 +1,10 @@
 set -ex
 
-TRAIN_EN=corpus/train.en
-TRAIN_JA=corpus/train.ja
-VALID_EN=corpus/valid.en
-VALID_JA=corpus/valid.ja
-TEST_EN=corpus/test.en
+TRAIN_EN=/Machine_Translation_Proto/corpus/train.en
+TRAIN_JA=/Machine_Translation_Proto/corpus/train.ja
+VALID_EN=/Machine_Translation_Proto/corpus/valid.en
+VALID_JA=/Machine_Translation_Proto/corpus/valid.ja
+TEST_EN=/Machine_Translation_Proto/corpus/test.en
 
 cat $TRAIN_EN $TRAIN_JA > train.enja
 python src/sp_learn.py --input train.enja --prefix bpe --vocab-size 4000 --character-coverage 0.9995 --threads 1
