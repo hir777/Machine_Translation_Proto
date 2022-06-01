4891a11b1a037d184dba527f1cd909a16f8acbf5
hiro <69237236+hir777@users.noreply.github.com>
2022-06-01 21:56:34 +0900

    Create download_dataset.py

---
+++ download_dataset.py
@@ -0,0 +1,9 @@
+import os
+
+dir_data = "~/content/"
+if not os.path.exists(dir_data):
+  os.mkdir(dir_data)
+os.chdir(dir_data)
+os.system('wget　https://www.kecl.ntt.co.jp/icl/lirg/jparacrawl/release/3.0/pretrained_models/en-ja/small.tar.gz')
+os.system('tar -xf small.tar.gz')
+os.system('rm small.tar.gz')
