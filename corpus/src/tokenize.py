edd2eb787b5c1267cc54ab863dcf6307d83f95fc
hiro <69237236+hir777@users.noreply.github.com>
2022-06-07 00:36:58 +0900

    Create tokenize.py

---
+++ corpus/src/tokenize.py
@@ -0,0 +1,26 @@
+import sys
+import re
+from sacremoses import MosesTokenizer
+import unicodedata
+import spacy
+
+
+def tokenize_en(en):
+    mt = MosesTokenizer(lang='en')
+
+    en = unicodedata.normalize('NFKC', en)
+    en = re.sub(mt.AGGRESSIVE_HYPHEN_SPLIT[0], r'\1 - ', en)
+    en = mt.tokenize(en, escape=False)
+    en = ' '.join(en)
+    en = en.lower()
+    return en
+
+
+def tokenize_ja(ja):
+    gt = spacy.load('ja_ginza')
+
+    ja = unicodedata.normalize('NFKC', ja)
+    ja = gt.tokenize(ja)
+    ja = [token.text for token in ja]
+    ja = ' '.join(ja)
+    return ja
