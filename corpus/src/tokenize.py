20f4c6f70c811d8e267c28987da73420e740735a
hiro <69237236+hir777@users.noreply.github.com>
2022-06-08 00:51:53 +0900

    Update tokenize.py

--- corpus/src/tokenize.py
+++ corpus/src/tokenize.py
@@ -1,13 +1,11 @@
-import sys
 import re
-from sacremoses import MosesTokenizer
+import sacremoses as sm
 import unicodedata
 import spacy
 
 
 def tokenize_en(en):
-    mt = MosesTokenizer(lang='en')
-
+    mt = sm.MosesTokenizer(lang='en')
     en = unicodedata.normalize('NFKC', en)
     en = re.sub(mt.AGGRESSIVE_HYPHEN_SPLIT[0], r'\1 - ', en)
     en = mt.tokenize(en, escape=False)
@@ -18,7 +16,6 @@ def tokenize_en(en):
 
 def tokenize_ja(ja):
     gt = spacy.load('ja_ginza')
-
     ja = unicodedata.normalize('NFKC', ja)
     ja = gt.tokenize(ja)
     ja = [token.text for token in ja]
