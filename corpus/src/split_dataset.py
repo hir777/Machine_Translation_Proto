311214164297027f6e821ec7ca6e0ac8ef0fd04a
hiro <69237236+hir777@users.noreply.github.com>
2022-06-07 22:34:24 +0900

    Update split_dataset.py

--- corpus/src/split_dataset.py
+++ corpus/src/split_dataset.py
@@ -3,6 +3,7 @@ from regex import F
 from tqdm import tqdm
 from typing import Dict
 import numpy as np
+import os
 
 
 def check_ratio(split_ratio: Dict[str, float]):
@@ -14,34 +15,30 @@ def check_ratio(split_ratio: Dict[str, float]):
     return False if len(ratio) != 3 or not all([val > 0.0 for val in vals]) or not (1.0 - eps < sum < 1.0 + eps) else True
 
 
-def split_dataset(en_sents, ja_sents, split_ratio: Dict[str, float]):
-    train_en, train_ja, dev_en, dev_ja, test_en, test_ja = [], [], [], [], [], []
+def write_ds(f_name, f_path, bitexts):
+    en_path = os.path.join(f_path, "/{}.en".format(f_name))
+    ja_path = os.path.join(f_path, "/{}.ja".format(f_name))
+    with open(en_path, 'w') as f_en, open(ja_path, 'w') as f_ja:
+        for bitext in tqdm(bitexts):
+            en, ja = bitext.split('\t')
+            f_en.write(en + '\n')
+            f_ja.write(ja + '\n')
+
+
+def split_dataset(en_sents, ja_sents, split_ratio: Dict[str, float], repo_path):
+    data_path = os.path.join(repo_path, "/corus/data")
     en_ja = [en + '\t' + ja for en,
              ja in tqdm(zip(en_sents, ja_sents), total=len(en_sents))]
     rd.shuffle(en_ja)
 
     size = len(en_ja)
-    if not check_ratio:
-        return
-    train_size = split_ratio["train"] * size
-    dev_size = split_ratio["dev"] * size
-    train = en_ja[:train_size]
-    dev = en_ja[train_size:train_size + dev_size]
-    test = en_ja[train_size + dev_size:]
+    if check_ratio:
+        train_size = split_ratio["train"] * size
+        valid_size = split_ratio["valid"] * size
+        train = en_ja[:train_size]
+        valid = en_ja[train_size:train_size + valid_size]
+        test = en_ja[train_size + valid_size:]
 
-    for bitext in tqdm(train):
-        en, ja = bitext.split('\t')
-        train_en.append(en)
-        train_ja.append(ja)
-
-    for bitext in tqdm(dev):
-        en, ja = bitext.split('\t')
-        dev_en.append(en)
-        dev_ja.append(ja)
-
-    for bitext in tqdm(test):
-        en, ja = bitext.split('\t')
-        test_en.append(en)
-        test_ja.append(ja)
-
-    return train_en, train_ja, dev_en, dev_ja, test_en, test_ja
+        write_ds('train', data_path, train)
+        write_ds('valid', data_path, valid)
+        write_ds('test', data_path, test)
