84a59304ca3de84adb0969ee5d0cf799cb2657b4
hiro <69237236+hir777@users.noreply.github.com>
2022-06-07 23:25:37 +0900

    Update split_dataset.py

--- corpus/src/split_dataset.py
+++ corpus/src/split_dataset.py
@@ -16,8 +16,8 @@ def check_ratio(split_ratio: Dict[str, float]):
 
 
 def write_ds(f_name, f_path, bitexts):
-    en_path = os.path.join(f_path, "/{}.en".format(f_name))
-    ja_path = os.path.join(f_path, "/{}.ja".format(f_name))
+    en_path = os.path.join(f_path, "{}.en".format(f_name))
+    ja_path = os.path.join(f_path, "{}.ja".format(f_name))
     with open(en_path, 'w') as f_en, open(ja_path, 'w') as f_ja:
         for bitext in tqdm(bitexts):
             en, ja = bitext.split('\t')
@@ -26,7 +26,7 @@ def write_ds(f_name, f_path, bitexts):
 
 
 def split_dataset(en_sents, ja_sents, split_ratio: Dict[str, float], repo_path):
-    data_path = os.path.join(repo_path, "corpus/data")
+    data_path = os.path.join(repo_path, "corpus/data/")
     en_ja = [en + '\t' + ja for en,
              ja in tqdm(zip(en_sents, ja_sents), total=len(en_sents))]
     rd.shuffle(en_ja)
