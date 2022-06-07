b225fb4e8e9b246d90dfb27815cc11f273b82767
hiro <69237236+hir777@users.noreply.github.com>
2022-06-08 00:41:59 +0900

    Update create_dataset.py

--- corpus/src/create_dataset.py
+++ corpus/src/create_dataset.py
@@ -1,21 +1,19 @@
-import os
-from tqdm import tqdm
-from dl_tatoeba import dl_tatoeba
-from dl_tatoeba import json2list
-from tokenize import tokenize_en
-from tokenize import tokenize_ja
+import dl_tatoeba as dl
+import tokenize as tk
 import filter as fl
-from split_dataset import split_dataset
+import split_dataset as spl
+import tqdm as t
+
 
 if __name__ == "__main__":
-    repo_path = "/home/hiroshi/tmp/Machine_Translation_Proto/"
+    repo_path = "/Machine_Translation_Proto/"
     # データセットをダウンロードしてリスト化する
-    dl_tatoeba(repo_path)
-    en_ls, ja_ls = json2list(repo_path)
+    dl.dl_tatoeba(repo_path)
+    en_ls, ja_ls = dl.json2list(repo_path)
 
     # 英文と日本文をそれぞれトークン化する
-    en_ls = [tokenize_en(en) for en in tqdm(en_ls)]
-    ja_ls = [tokenize_ja(ja) for ja in tqdm(ja_ls)]
+    en_ls = [tk.tokenize_en(en) for en in t.tqdm(en_ls)]
+    ja_ls = [tk.tokenize_ja(ja) for ja in t.tqdm(ja_ls)]
 
     # フィルタリング
     en_ls, ja_ls = fl.len_filter(en_ls, ja_ls, 4, 16, truncate=True)
@@ -23,4 +21,4 @@ if __name__ == "__main__":
     en_ls, ja_ls = fl.ratio_filter(en_ls, ja_ls)
 
     split_ratio = {"train": 0.8, "valid": 0.1, "test": 0.1}
-    split_dataset(en_ls, ja_ls, split_ratio, repo_path)
+    spl.split_dataset(en_ls, ja_ls, split_ratio, repo_path)
