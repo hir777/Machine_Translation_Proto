0636fdb05f52e61380a8b6da33064b651ea262f3
hiro <69237236+hir777@users.noreply.github.com>
2022-06-09 09:12:30 +0900

    Update translate.py

--- src/translate.py
+++ src/translate.py
@@ -3,33 +3,28 @@ import unicodedata
 import sentencepiece as spm
 from fairseq.models.transformer import TransformerModel
 import argparse
+from sacremoses import MosesTokenizer
 
 
-def preprocess_en(en, path_bpe_model, alpha=None):
-    sp = spm.SentencePieceProcessor()
-    sp.Load(path_bpe_model)
-
+def preprocess_en(en, sp, mt):
     en = unicodedata.normalize('NFKC', en)
-    en = en.strip()
-    en = ' '.join(en.split())
-    if alpha is None:
-        en = sp.encode(en, out_type=int)
-    else:
-        en = sp.encode(en, out_type=int, enable_sampling=True, alpha=alpha)
-    en = ' '.join([sp.IdToPiece(i) for i in en])
+    en = re.sub(mt.AGGRESSIVE_HYPHEN_SPLIT[0], r'\1 - ', en)
+    en = mt.tokenize(en, escape=False)
+    en = en.strip().lower()
+    en = ' '.join(sp.encode(en, out_type="str"))
     return en
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--checkpoint_dir", type=str, default="../checkpoints/",
+    parser.add_argument("--checkpoint_dir", type=str, defalut="../checkpoints/",
                         help="path to the directory which stores checkpoint files")
     parser.add_argument("--checkpoint_file", type=str, default="checkpoint_last.pt",
                         help="a checkpoint file to be loaded  e.g. checkpoint_best.pt  checkpoint_last.pt")
     parser.add_argument("--data_name_or_path", type=str, default="../data-bin",
                         help="a file's name or path where data are stored")
     parser.add_argument("--path_bpe_model", type=str, default="../bpe.model")
-    parser.add_argument("--en", type=str, default="Hello World.")
+    parser.add_argument("en", type=str, default="Hello World.")
     args = parser.parse_args()
 
     model = TransformerModel.from_pretrained(
@@ -37,8 +32,11 @@ if __name__=="__main__":
         checkpoint_file=args.checkpoint_file,
         data_name_or_path=args.data_name_or_path)
 
-    en = preprocess_en(args.en, args.path_bpe_model)
-    ja = model.translate(args.en, beam=1, lenpen=0.6)
+    mt = MosesTokenizer(lang="en")
+    sp = spm.SentencePieceProcessor(model_file=args.path_bpe_model)
+
+    en = preprocess_en(args.en, sp, mt)
+    ja = model.translate(args.en, beam=5, lenpen=0.6)
     ja = ''.join(ja.split()).replace('▁', '').strip()
 
     print(ja)
