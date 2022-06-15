af6089890cd4a3b829a227763f7ff389cd748542
hiro <69237236+hir777@users.noreply.github.com>
2022-06-15 17:03:58 +0900

    Update tokenize_enja.py

--- corpus/src/tokenize_enja.py
+++ corpus/src/tokenize_enja.py
@@ -2,26 +2,77 @@ import re
 import sacremoses as sm
 import unicodedata
 import MeCab
-import unidic
+import threading
+from typing import List
 
 
-def tokenize_en(en):
-    mt = sm.MosesTokenizer(lang='en')
-    en = unicodedata.normalize('NFKC', en)
-    en = re.sub(mt.AGGRESSIVE_HYPHEN_SPLIT[0], r'\1 - ', en)
-    en = mt.tokenize(en, escape=False)
-    en = ' '.join(en)
-    en = en.lower()
-    return en
+class Tokenization():
+    def __init__(self, lang, threading=False, num_threads=4):
+        self.lang = lang
+        self.threading = threading
+        if lang == "en":
+            self.tokenizer = sm.MosesTokenizer(lang='en')
+        elif lang == "ja":
+            self.tokenizer = MeCab.Tagger("-Owakati")
+        else:
+            raise ValueError(
+                "Error: Language %s is not supported." % self.lang)
+        self.num_threads = num_threads
+        self.out = []   # 各部分リストをトークン化した時の結果をまとめて保持するためのリスト
+
+    def tokenize_en(self, en_sents: List[str]):
+        en_ls = []
+        for en in en_sents:
+            en = unicodedata.normalize("NFKC", en)
+            en = re.sub(
+                self.tokenizer.AGGRESSIVE_HYPHEN_SPLIT[0], r'\1 - ', en)
+            en = self.tokenizer.tokenize(en, escape=False)
+            en = ' '.join(en).lower()
+            en_ls.append(en)
+        self.out.append(en_ls)
+
+    def tokenize_ja(self, ja_sents: List[str]):
+        ja_ls = []
+        for ja in ja_sents:
+            ja = unicodedata.normalize("NFKC", ja)
+            ja = self.tokenizer.parse(ja)
+            ja_ls.append(ja)
+        self.out.append(ja_ls)
+
+    def tokenize(self, sents: List[str]):
+        num_sents = len(sents)
+        size = int(num_sents / self.num_threads)
+        threads = []
+        tgt = self.tokenize_en if self.lang == "en" else self.tokenize_ja
+        if self.threading:
+            tgt([sents[-1]])
+            for i in range(self.num_threads):
+                head = i * size
+                tail = (i+1) * size if i != (self.num_threads-1) else -1
+                thread = threading.Thread(target=tgt, args=[sents[head: tail]])
+                threads.append(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+        else:
+            tgt(sents)
+
+        res = [sent for sents in self.out for sent in sents]
+        return res
 
 
-def tokenize_ja(ja):
-    wakati = MeCab.Tagger('-Owakati')
-    ja = unicodedata.normalize('NFKC', ja)
-    ja = wakati.parse(ja)
-    return ja
+if __name__ == "__main__":
+    ja_ls = ["何かしてみましょう。",
+             "私は眠らなければなりません。",
+             "そろそろ寝なくちゃ。",
+             "今日は６月１８日で、ムーリエルの誕生日です！",
+             "ムーリエルは２０歳になりました。",
+             "パスワードは「Muiriel」です。",
+             "まもなく私は戻って来ます。",
+             "私は言葉に詰まった。"
+             ]
 
-if __name__=="__main__":
-    ja = "くるまでまつ"
-    res = tokenize_ja(ja)
-    print(res)
+    t = Tokenization(lang="ja", threading=True)
+    out = t.tokenize(ja_ls)
+    print(''.join(out))
