09e6a1f2d0c04e26968e01a7b7e78f68cf32c535
hiro <69237236+hir777@users.noreply.github.com>
2022-06-24 23:03:14 +0900

    Update tokenize_enja.py

--- corpus/src/tokenize_enja.py
+++ corpus/src/tokenize_enja.py
@@ -5,41 +5,35 @@ import MeCab
 from typing import List
 import multiprocessing as mp
 import os
+import gc
 
 
 class Tokenization():
-    def __init__(self, multiproc=False, num_procs=4):
-        self.multiproc = mp.Value('i', int(multiproc))
-        self.num_procs = mp.Value('i', num_procs)
+    def __init__(self, workers=1):
+        self.workers = mp.Value('i', workers)
 
     def tokenize_en(self, en_sents: List[str]):
         mt = sm.MosesTokenizer(lang='en')
-        en_ls = []
         for en in en_sents:
             en = unicodedata.normalize("NFKC", en)
             en = re.sub(
                 mt.AGGRESSIVE_HYPHEN_SPLIT[0], r'\1 - ', en)
             en = mt.tokenize(en, escape=False)
             en = ' '.join(en).lower()
-            en_ls.append(en)
-        return en_ls
+            yield en
 
     def tokenize_ja(self, ja_sents: List[str]):
         mecab = MeCab.Tagger("-Owakati")
-        ja_ls = []
         for ja in ja_sents:
             ja = unicodedata.normalize("NFKC", ja)
             ja = mecab.parse(ja)
-            ja_ls.append(ja)
-        return ja_ls
+            yield ja
 
     def tokenize_en_ja(self, queue, en_sents: List[str], ja_sents: List[str]):
-        print("Tokenization [Process ID: {}] started.".format(
+        print("Tokenization (Process ID: {}) started.".format(
             os.getpid()))
-        en_ls = self.tokenize_en(en_sents)
-        ja_ls = self.tokenize_ja(ja_sents)
         queue.put([en.replace('\t', '') + '\t' + ja.replace('\t', '')
-                   for en, ja in zip(en_ls, ja_ls)])
+                   for en, ja in zip(self.tokenize_en(en_sents), self.tokenize_ja(ja_sents))])
         print("Tokenization [Process ID: {}] has finished.".format(
             os.getpid()))
 
@@ -50,26 +44,27 @@ class Tokenization():
         """
         queue = mp.Queue()
         num_sents = min(len(en_sents), len(ja_sents))
-        size = int(num_sents / self.num_procs.value)
-        if self.multiproc.value:
-            tail = 0
-            for i in range(self.num_procs.value):
-                head = size * i
-                tail = size * \
-                    (i+1) if i != (self.num_procs.value-1) else num_sents
-                proc = mp.Process(target=self.tokenize_en_ja, args=[queue,
-                                                                    en_sents[head: tail], ja_sents[head: tail]])
-                proc.start()
+        min_workers = 1
+        max_workers = 12
 
-        else:
-            self.num_procs.value = 1
-            self.tokenize_en_ja(
-                queue, en_sents[:num_sents], ja_sents[:num_sents])
+        self.workers.value = self.workers.value if min_workers <= self.workers.value and self.workers.value <= max_workers else min_workers
+        self.workers.value = self.workers.value if self.workers.value <= num_sents else min_workers
 
-        out = []
-        for i in range(self.num_procs.value):
-            out.append(queue.get())
-        bitexts = [sent for sents in out for sent in sents]
+        size = int(num_sents / self.workers.value)
+        for idx in range(self.workers.value):
+            head = idx * size
+            tail = (idx+1) * size if idx != (self.workers.value-1) else num_sents
+            proc = mp.Process(target=self.tokenize_en_ja, args=[queue,
+                                                                en_sents[head: tail], ja_sents[head: tail]])
+            proc.start()
+
+        tmp = []
+        for _ in range(self.workers.value):
+            tmp.append(queue.get())
+        tmp_gen = (sents for sents in tmp)
+        bitexts = [sent for sents in tmp_gen for sent in sents]
+        del tmp[:]
+        gc.collect()
         en_ls, ja_ls = [], []
         for bitext in bitexts:
             en, ja = bitext.split('\t')
@@ -77,6 +72,7 @@ class Tokenization():
             ja_ls.append(ja.strip())
 
         return en_ls, ja_ls
+
 
 # テスト用コード
 if __name__ == "__main__":
@@ -90,7 +86,7 @@ if __name__ == "__main__":
              "まもなく私は戻って来ます。"
              ]
 
-    t = Tokenization(multiproc=True, num_procs=2)
-    en_sents, ja_sents = t.tokenize(en_ls, ja_ls)
+    tkn = Tokenization(workers=2)
+    en_sents, ja_sents = tkn.tokenize(en_ls, ja_ls)
     print(en_sents)
     print(ja_sents)
